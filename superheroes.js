09f81a27cf6c93c0e0c74df9a05442356235c957
Henry Newman <hnewman@ccsuvt.org>
2016-03-04 10:23:52 -0500

    Henry characteristics

--- superheroes.js
+++ superheroes.js
@@ -11,3 +11,18 @@ var superheroes = [{
   Color: "Purple",
   Superpower: "Archery"
 }];
+
+showList();
+function showList(){
+  var headings = Object.keys(superheroes[0]);
+  for(var i in headings){
+    document.getElementById("headings").innerHTML+='<p>' + headings[i] + '</p>';
+  }
+}
+
+function toggleList(){
+  var headings = document.getElementById("headings");
+  if (headings.style.display == "none"){
+    headings.style.display = "";
+  } else headings.style.display = "none";
+};
