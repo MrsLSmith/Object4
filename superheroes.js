d662b86ddba5fc32e84e502d7cdf318ccbe17621
LPotasiewicz <lpotasiewicz@ccsuvt.org>
2016-03-04 10:23:52 -0500

    luke tables js and html

--- superheroes.js
+++ superheroes.js
@@ -11,3 +11,17 @@ var superheroes = [{
   Color: "Purple",
   Superpower: "Archery"
 }];
+showTable();
+function showTable(){
+  var table = '';
+  for(var i = 0; i < 3; i++){
+    table += "<tr><td>" + superheroes[i].Name + "</td><td>" + superheroes[i].Color + "</td><td>" + superheroes[i].Superpower + "</td></tr>";
+    document.getElementById("Table").innerHTML= table;
+  }
+};
+function toggleTable(){
+  var table = document.getElementById("Table");
+  if(table.style.display == "none"){
+    table.style.display = "";
+  }else table.style.display = "none";
+};
