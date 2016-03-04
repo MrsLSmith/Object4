91ccf7441a32847fb573503fa2f31aa93d839921
Ivan <iseyller@ccsuvt.org>
2016-03-04 10:24:13 -0500

    superheroes list

--- superheroes.js
+++ superheroes.js
@@ -1,3 +1,4 @@
+
 var superheroes = [{
   Name: "Incredible Hulk",
   Color: "Green",
@@ -11,3 +12,12 @@ var superheroes = [{
   Color: "Purple",
   Superpower: "Archery"
 }];
+
+function toggleList(){
+  var superheroes
+}
+
+
+for(var i in superheroes){
+ document.getElementById("Superheroes").innerHTML += "<p>" + superheroes[i].Name + "</p>";
+}
